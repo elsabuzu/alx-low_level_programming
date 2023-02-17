8a710d4a0975ce71b8aec5786d518a4907da7869
elsa <elsabuzuneh@gmail.com>
2023-02-17 15:15:18 -0800

    size of types

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,12 @@
+#!/bin/bash
+#include <stdio.h>
+
+int main (void)
+{
+	printf("size of a char:\n",size0f(char));
+	printf("size of an int:\n",sizeof(int));
+	printf("size of a long int:\n",sizeof(long int));
+	printf("size of a long long int\n",sizeof(long long int));
+	printf("size of a float\n",sizeof(float));
+	return 0;
+}
