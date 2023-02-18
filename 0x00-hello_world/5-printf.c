927cff2889ae38f653472c78076acfe43fe8461a
elsa <elsabuzuneh@gmail.com>
2023-02-17 17:16:52 -0800

    printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -4,7 +4,7 @@
  * main ->prints exactly with proper grammar,but
  * the  outcome is a piece of art,
  *
- * Return : it always return 0 value.
+ * Return: it always return 0 value.
  */
 int main(void)
 {
