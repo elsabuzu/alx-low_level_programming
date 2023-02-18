231bc2b01e3b8b45dfa7ca1204fd0e1eec38da83
elsa <elsabuzuneh@gmail.com>
2023-02-17 17:36:16 -0800

    quote

---
+++ 0x00-hello_world/101.quote.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+/**
+ * main ->prints exactly and that piece of art is useful"
+ *  - Dora Kopar , 2015-10-19,
+ *
+ * return: prints always zero value.
+ */
+int main(void)
+{
+	printf("and that piece of art is useful\" -Dora Kopar , 2015-10-19,");
+
+	return (0);
+}
