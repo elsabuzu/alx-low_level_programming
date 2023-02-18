43cef94861b3401621b428c111df2800f01bb104
elsa <elsabuzuneh@gmail.com>
2023-02-17 17:14:51 -0800

    printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main -> prints exactly with proper grammar, but 
+ * main ->prints exactly with proper grammar,but
  * the  outcome is a piece of art,
  *
  * Return : it always return 0 value.
