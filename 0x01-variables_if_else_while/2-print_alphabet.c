3f611405ca1f33903cac211020708ce480c08241
elsa <elsabuzuneh@gmail.com>
2023-02-18 17:40:13 -0800

    lowercase

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,23 @@
+#include<stdlib.h>
+#include<time.h>
+#include<stdio.h>
+
+/**
+ * main - prints lower case alphabet
+ * Return - zero.
+ */
+int main(void)
+{
+	char n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+
+	for ( n = 'a'; n <= 'z'; n++)
+	{
+		putchar (n);
+		putchar ('\n');
+	}
+
+	return (0);
+}
