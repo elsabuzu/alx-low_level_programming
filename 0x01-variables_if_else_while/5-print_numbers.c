45f719465f1f97b948d7b56ffbc8751ef3421ba6
elsa <elsabuzuneh@gmail.com>
2023-02-18 19:08:47 -0800

    1 upto 10

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -2,16 +2,14 @@
 
 /*
  * main - print number 1 upto 10
- * Return: zero.
+ * Return: always zero.
  */
 int main(void)
 {
 	int num;
 
 	for (num = 0; num < 10; num++)
-	{
 		printf("%d", num);
-	}
 
 	printf("\n");	
 	
