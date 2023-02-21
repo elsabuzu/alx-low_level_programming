797dbba4e31016a88a2936a17222944ad2cfae5c
elsabet <58572476+elsabuzu@users.noreply.github.com>
2023-02-21 22:43:59 +0000

    Update main.h

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,7 +1,7 @@
 void print_alphabet(void);
 int _putchar(char c);
 void print_alphabet_x10(void);
-int _isslower( int c);
+int _islower( int c);
 int _isalphaa(int c);
 int print_sign(int n);
 int _abs(int);
