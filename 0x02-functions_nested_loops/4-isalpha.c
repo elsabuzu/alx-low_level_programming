d81e20fbf573fac08472812b851a5792c9b696bc
elsa <elsabuzuneh@gmail.com>
2023-02-21 15:30:36 -0800

    check

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _isalpha - checks for alphabetic letter ,lowercase or uppercaser.
+ * @c: character in ASCII code.
+ * Return: 1 for letters , 0 otherwise.
+ */
+int _isalpha(int c)
+{
+	if((c >= 97 && c <= 122) || (c >= 65 && c <= 90))
+	{
+		return (1);
+	}
+	else
+		return (0);
+}
