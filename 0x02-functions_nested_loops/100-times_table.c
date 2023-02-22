85487236bff6aafc555c9c02166df08939bc3b1c
elsa <elsabuzuneh@gmail.com>
2023-02-22 11:55:54 -0800

    times

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -36,7 +36,7 @@ void print_times_table(int n)
 				else
 				{
 					_putchar(((prod / 100) % 10) + 48);
-					_putchar(((prod / 100) % 10) + 48);
+					_putchar(((prod / 10) % 10) + 48);
 					_putchar((prod % 10) + 48);
 				}
 
