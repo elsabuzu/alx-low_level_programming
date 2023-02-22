6e14c94008583ea0d75b43303862aa988ead20cd
elsa <elsabuzuneh@gmail.com>
2023-02-22 11:09:23 -0800

    natural numbers upto 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -2,23 +2,23 @@
 /**
  * print_to_98 - prints all natural numbers starts from n to 98.
  *
- * @n:input number. 
+ *@n:input number.
  */
 void print_to_98(int n)
 {
-	if(n >= 98)
+	if (n >= 98)
 	{
-		for(; n <98; n--)
+		for (; n < 98; n--)
 		{
-			printf("%d, ", n);
+			printf ("%d, ", n);
 		}
 	}
 	else if (n < 98)
 	{
 		for (; n < 98; n++)
 		{
-			printf("%d, ",n);
+			printf("%d, ", n);
 		}
 	}
-	printf("%d\n",n);
+	printf ("%d\n", n);
 }
