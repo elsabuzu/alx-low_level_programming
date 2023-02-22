452997ad9f6a366cff908e64b547ee36dc7cea0d
elsa <elsabuzuneh@gmail.com>
2023-02-22 05:57:14 -0800

    9x time

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+void times_table(void)
+{
+	int a, b, prod;
+
+	for (a = 0; a <= 9; a++)
+	{
+		_putchar(48);
+
+		for (b = 0; b <= 9; b++)
+		{
+			prod = a * b;
+
+			_putchar(44);
+		        _putchar(32);
+			
+			if (prod <= 9)
+			{
+				_putchar(32);
+				_putchar(prod + 48);
+			}
+			
+			else
+				_putchar((prod / 10) + 48);
+			
+			_putchar((prod % 10) + 48);
+		}
+	}
+	
+	_putchar('\n');
+}
