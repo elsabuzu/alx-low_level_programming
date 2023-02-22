d57aa034dabbc156c892abf4af0d70557f9c67ac
elsa <elsabuzuneh@gmail.com>
2023-02-22 14:43:41 -0800

    50 fibonacci number

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+/**
+ * main-print sum of even-valued terms.
+ * Return: always 0.
+ */
+int main(void)
+{
+	unsigned long n1 = 1, n2 = 2, fn;
+	float sum;
+
+	while(fn <= 4000000)
+	{
+		fn = n1 + n2;
+		n1 = n2;
+		n2 = fn;
+		if ((n1 % 2) == 0)
+			sum += n1;
+	}
+	printf("%ld\n", afn);
+	return (0);
+}
