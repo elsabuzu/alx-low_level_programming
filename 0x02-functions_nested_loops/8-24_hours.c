b5acf1d7a57feeabe993df04979e987c9d90a074
elsa <elsabuzuneh@gmail.com>
2023-02-22 05:13:37 -0800

    minute

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -6,18 +6,28 @@
  */
 void  jack_bauer(void)
 {
-	int hr, min;
+	int a, b, c, d;
 
-	for (hr = 0; hr <= 23; hr++)
+	for (a = 0; a <= 2; a++)
 	{
-		for (min = 0; min <= 59; min++)
+		for (b = 0; b <= 9; b++)
 		{
-			_putchar((hr / 10) + '0');
-			_putchar((hr & 10) + '0');
-			_putchar(':');
-			_putchar((min / 10) + '0');
-			_putchar((min % 10) + '0');
-			_putchar('\n');
+			if ((a <= 1 && b <= 9) || (a <= 2 && b <= 3))
+			{
+				for (c = 0; c <= 5; c++)
+				{
+					for (d =0; d <= 9; d++)
+					{
+						_putchar(a  + '0');
+						_putchar(b  + '0');
+						_putchar(':');
+						_putchar(c + '0');
+						_putchar(d + '0');
+						_putchar('\n');
+					}
+				}
+			}
 		}
+
 	}
 }
