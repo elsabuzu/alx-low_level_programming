6a1076d4dcf14ecf120efc1722dfdc1d67dc5f65
elsa <elsabuzuneh@gmail.com>
2023-02-22 16:19:14 -0800

    fibonacci

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -38,8 +38,8 @@ int main(void)
 			n2 = fn;
 			n22 = (fn2 % 1000000000);
 		}
-		if(((n1 + n2) < 0) && b == 1)
-			b = 0;
+		if(((n1 + n2) < 0) && b1 == 1)
+			b1 = 0;
 	}
 	printf("\n");
 	return (0);
