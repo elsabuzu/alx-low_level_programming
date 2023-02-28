a2d4b68426b8ac0f993f40c547dede5a59836f22
elsa <elsabuzuneh@gmail.com>
2023-02-28 06:42:31 -0800

    revers string

---
+++ 0x05-pointers_arrays_strings/4-print_dev.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_rev - print a string to reverse,
+ * followed by a  new  line.
+ * @s: pointer to a string. 
+ */
+
+void print_rev(char *s)
+{
+	int i=0;
+
+	while (s[i])
+	{
+		i++;
+	}
+
+	while (i--)
+	{
+		_putchar(s[i]);
+	}
+
+	_putchar('\n');
+}
