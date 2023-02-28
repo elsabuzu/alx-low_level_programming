c697d68636165a7e9c3b9af3040992e512e936df
elsa <elsabuzuneh@gmail.com>
2023-02-28 15:49:37 -0800

    reverse

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -6,18 +6,18 @@
 
 void rev_string(char *s)
 {
-	int i, j, temp;
+	int i, j, temp; 
 	i = 0;
 	j = 0;
 
-	while (s[i] != 0)
+	while (s[i] != '\0')
 	{
 		j++;
 	}
 
 
 	j--;
-	while (j>i)
+	while (j > i)
 	{
 		temp = s[j];
 		s[j--] = s[i];
