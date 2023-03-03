f04a7ebd06108cb247e47fd4ce30d726ed731e47
elsa <elsabuzuneh@gmail.com>
2023-03-03 05:23:13 -0800

    concatenate

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int n1 = 0, n2 = 0;
+
+	while (*(dest + n1) != '\0')
+	{
+		n1++;
+	}
+
+	while (n2 < n)
+	{
+		*(dest + n1) =*(dest + n2);
+		if(*(src + n2) == '\0')
+			break;
+		n1++;
+		n2++;
+	}
+	return (dest);
+}
