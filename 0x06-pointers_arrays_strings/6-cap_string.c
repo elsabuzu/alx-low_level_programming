4fcd14263d371c9a0b63536753128cb072ed15c8
elsa <elsabuzuneh@gmail.com>
2023-03-03 09:24:46 -0800

    capital string

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,33 @@
+#include "main.h"
+/**
+ * cap_string -capitalize strings.
+ * @c:string
+ * Return:address of string.
+ */
+
+char *cap_string(char *c)
+{
+	int i = 0, j;
+	char a[] = " \t\n,;.!?\"(){}";
+
+	while (*(c + i))
+	{
+		if  (*(c + i) >= 'a' && *(c + i) <= 'z')
+		{
+			if ( i == 0)
+			{
+				*(c + i) -= 'a' - 'A';
+			}
+			else
+			{
+				for (j = 0; j <= 12; j++)
+				{
+					if (a[j] == *(c + i -1))
+						*(c + i) -= 'a' - 'A';
+				}
+			}
+		}
+		i++;
+	}
+	return (c);
+}
