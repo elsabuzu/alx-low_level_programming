a95afbada3007c876c4a8be13dfae0cdd4a536cc
elsa <elsabuzuneh@gmail.com>
2023-03-03 09:53:49 -0800

    encode

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * leet - encoding a string to 1337.
+ * @l - string.
+ * Return: address of l.
+ */
+
+char *leet(char *l)
+{
+	int i, j;
+	char a[] = "aAeEoOtTlL";
+	char b[] = "4433007711";
+
+	for (i = 0; *(l + i); i++)
+	{
+		for ( j = 0; j <= 9; j++)
+		{
+			if (a[j] == l[i])
+			{
+				l[i] = b[j];
+			}
+		}
+	}
+	return (l);
+}
+
