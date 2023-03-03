18d942c2d0b4491155bc531d7391e4e91ddc86c2
elsa <elsabuzuneh@gmail.com>
2023-03-03 08:21:59 -0800

    reverse

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ */
+
+void reverse_array(int *a, int n)
+{
+	int i, j, rev;
+
+	for (i = 0;i < n - 1; i++)
+	{
+		for (j = i + 1; j > 0; j--)
+		{
+			rev = *(a + j);
+			*(a + j) = *(a + (j + 1));
+			*(a + (j - 1)) = rev;
+		}
+	}
+}
