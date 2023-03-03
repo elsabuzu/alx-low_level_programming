13a661d35da1b47367222170cba24f4b6dd94d99
elsa <elsabuzuneh@gmail.com>
2023-03-03 10:11:59 -0800

    rot13

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * rot13 - encode a string using rot13.
+ * @r: string.
+ * Return: address of r.
+ */
+
+char *rot13(char *r)
+{
+	int i, j;
+	char a[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char b[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABXDEFGHIJKLM";
+
+	for (i = 0; *(r + i); i++)
+	{
+		for (j = 0; j < 52; j++)
+		{
+			if (a[j] == *(r + i))
+			{
+				*(r + i) = b[i];
+				break;
+			}
+		}
+	}
+	return (r);
+}
